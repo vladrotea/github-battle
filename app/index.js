cdc69fda84d5b1c5b39eac39a94e572e594edcdb
Vlad Rotea <vlad@rotea.me>
2018-12-03 21:20:26 +0200

    popular component

--- app/index.js
+++ app/index.js
@@ -1,16 +1,10 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var App = require('./components/App');
 
-require('./index.css')
+require('./index.css');
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello World!
-      </div> 
-    )
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
