377ed5a8e6fd771c46863c8f2542c02b576da10e
Vlad Rotea <vlad.rotea@fortech.ro>
2018-12-06 14:44:44 +0200

    react router; added new components

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,19 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './app/index.js',
-  output: { 
+  output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
+    publicPath: '/',
   },
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
     ]
+  },
+  devServer: {
+      historyApiFallback: true
   },
   mode: 'development',
   plugins: [
