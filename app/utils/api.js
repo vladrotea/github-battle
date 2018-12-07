ed7555174ce98f282d7888b707d050961052bd65
goarga <goarga@stylyze.com>
2018-12-07 15:07:51 +0200

    fixed api

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,11 +1,9 @@
 var axios = require('axios')
 
-module.exports = {
-  fetchPopularRepos: function (language) {
-    var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories')
-    return axios.get(encodedURI)
-      .then(function (response) {
-        return response.data.items;
-      });
-  }
+export default fetchPopularRepos = (language) => {
+  var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories')
+  return axios.get(encodedURI)
+    .then(function (response) {
+      return response.data.items;
+    });
 }
